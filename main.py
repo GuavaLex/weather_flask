b2f14b0bc4dbaa694ae7e3b27c84b0816d2ab427
guava <guavareigen2@gmail.com>
2023-02-20 01:44:39 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/api/v1/<station>/<date>")
+def about(station,date):
+
+if __name__ == "main":
+    app.run(debug=True)
